a52d3adf533b32d7bab2c2153ec4a00120a48ecb
CoderMarido <contact@marido.host>
2018-09-06 21:18:54 +0200

    Updated to 2.1.1 (not too much manual changes).

--- src/nl/marido/deluxeheads/animation/Animations.java
+++
@@ -1,7 +0,0 @@
-package nl.marido.deluxeheads.animation;
-
-public class Animations {
-
-	// TODO: Add animation heads with live (cached) texures in the next updates.
-
-}
--- src/nl/marido/deluxeheads/animation/LiveData.java
+++
@@ -1,7 +0,0 @@
-package nl.marido.deluxeheads.animation;
-
-public class LiveData {
-
-	// TODO: Add animation heads with live (cached) texures in the next updates.
-
-}
